0de15c2b1f5513408dcf3e3da8c13293030d166d
onee-only <kimww0306@gmail.com>
2023-04-25 17:28:04 +0900

    ADD: go 파일 생성 명령어 추가

---
+++ createGo.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+protoc --go_out=../server/articles/pb --go_opt=paths=source_relative --go-grpc_out=../server/articles/pb --go-grpc_opt=paths=source_relative --experimental_allow_proto3_optional articles/service.proto
+protoc --go_out=../server/comments/pb --go_opt=paths=source_relative --go-grpc_out=../server/comments/pb --go-grpc_opt=paths=source_relative --experimental_allow_proto3_optional comments/service.proto
+protoc --go_out=../server/rank/pb --go_opt=paths=source_relative --go-grpc_out=../server/rank/pb --go-grpc_opt=paths=source_relative rank/service.proto
