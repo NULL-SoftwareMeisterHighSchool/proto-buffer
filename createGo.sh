af8c4532f55a30ebbd98e70ca0fe06e7d85b9b12
onee-only <kimww0306@gmail.com>
2023-04-26 18:11:24 +0900

    ADD: UserEvent 핸들러 추가

--- createGo.sh
+++ createGo.sh
@@ -3,3 +3,4 @@
 protoc --go_out=../server/articles/pb --go_opt=paths=source_relative --go-grpc_out=../server/articles/pb --go-grpc_opt=paths=source_relative --experimental_allow_proto3_optional articles/service.proto
 protoc --go_out=../server/comments/pb --go_opt=paths=source_relative --go-grpc_out=../server/comments/pb --go-grpc_opt=paths=source_relative --experimental_allow_proto3_optional comments/service.proto
 protoc --go_out=../server/rank/pb --go_opt=paths=source_relative --go-grpc_out=../server/rank/pb --go-grpc_opt=paths=source_relative rank/service.proto
+protoc --go_out=../server/users/pb --go_opt=paths=source_relative --go-grpc_out=../server/users/pb --go-grpc_opt=paths=source_relative users/service.proto
