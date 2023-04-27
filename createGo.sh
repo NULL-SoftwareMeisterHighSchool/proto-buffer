37f5e8562a6a47fb4a3227f3e0a785dc7d687ce0
onee-only <kimww0306@gmail.com>
2023-04-27 18:16:51 +0900

    REFACTOR: out 디렉토리 변경

--- createGo.sh
+++ createGo.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-protoc --go_out=../server/articles/pb --go_opt=paths=source_relative --go-grpc_out=../server/articles/pb --go-grpc_opt=paths=source_relative --experimental_allow_proto3_optional articles/service.proto
-protoc --go_out=../server/comments/pb --go_opt=paths=source_relative --go-grpc_out=../server/comments/pb --go-grpc_opt=paths=source_relative --experimental_allow_proto3_optional comments/service.proto
-protoc --go_out=../server/rank/pb --go_opt=paths=source_relative --go-grpc_out=../server/rank/pb --go-grpc_opt=paths=source_relative rank/service.proto
-protoc --go_out=../server/users/pb --go_opt=paths=source_relative --go-grpc_out=../server/users/pb --go-grpc_opt=paths=source_relative users/service.proto
+protoc --go_out=../server/pb --go_opt=paths=source_relative --go-grpc_out=../server/pb --go-grpc_opt=paths=source_relative --experimental_allow_proto3_optional articles/service.proto
+protoc --go_out=../server/pb --go_opt=paths=source_relative --go-grpc_out=../server/pb --go-grpc_opt=paths=source_relative --experimental_allow_proto3_optional comments/service.proto
+protoc --go_out=../server/pb --go_opt=paths=source_relative --go-grpc_out=../server/pb --go-grpc_opt=paths=source_relative rank/service.proto
+protoc --go_out=../server/pb --go_opt=paths=source_relative --go-grpc_out=../server/pb --go-grpc_opt=paths=source_relative users/service.proto
