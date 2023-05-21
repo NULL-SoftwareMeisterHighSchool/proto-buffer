767190b52eb1f99d81e726c3775157271d1017d5
onee-only <kimww0306@gmail.com>
2023-05-22 08:11:50 +0900

    DELETE: rank 서비스 cmd 삭제

--- createGo.sh
+++ createGo.sh
@@ -2,5 +2,4 @@
 
 protoc --go_out=../server/pb --go_opt=paths=source_relative --go-grpc_out=../server/pb --go-grpc_opt=paths=source_relative --experimental_allow_proto3_optional articles/service.proto
 protoc --go_out=../server/pb --go_opt=paths=source_relative --go-grpc_out=../server/pb --go-grpc_opt=paths=source_relative --experimental_allow_proto3_optional comments/service.proto
-protoc --go_out=../server/pb --go_opt=paths=source_relative --go-grpc_out=../server/pb --go-grpc_opt=paths=source_relative rank/service.proto
 protoc --go_out=../server/pb --go_opt=paths=source_relative --go-grpc_out=../server/pb --go-grpc_opt=paths=source_relative users/service.proto
