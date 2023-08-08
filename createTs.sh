0fe8ee4b1774b9a6a1b14817f6b3ad5dface8906
onee-only <kimww0306@gmail.com>
2023-08-08 21:03:41 +0900

    FIX: 이전의 protoc으로

--- createTs.sh
+++ createTs.sh
@@ -4,23 +4,19 @@
 # scan all .proto from ./src/proto and compile them to ./src/libs/codegen/rpc.js & rpc.d.ts
 
 export DST="../types"
-
+export PBTS_PATH="/Users/oneee/.nvm/versions/node/v18.17.0/bin/protoc-gen-ts"
 rm -rf $DST
 mkdir -p $DST
 
 for name in "articles" "comments" "users"
 do
-    # npx pbjs --target static-module \
-    # --wrap commonjs \
-    # --keep-case \
-    # --path "articles" \
-    # --out "$DST/$name.js" \
+    # npx pbjs \
+    # --ts "$DST/$name.ts" \
     # ./$name/service.proto
 
-    # npx pbts --out "$DST/$name.d.ts" \
-    # ${DST}/$name.js
-
-    npx pbjs \
-    --ts "$DST/$name.ts" \
-    ./$name/service.proto
+    protoc \
+    --plugin="protoc-gen-ts=${PBTS_PATH}" \
+    --js_out="import_style=commonjs,binary:${DST}" \
+    --ts_out="service=grpc-web:${DST}" \
+    $name/service.proto
 done
