efb95b8e04ac4c4e9d207171ab297c29c0ef396d
onee-only <kimww0306@gmail.com>
2023-08-19 16:06:55 +0900

    createGo 변경

--- createGo.sh
+++ createGo.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+export GO_PATH=~/go
+export PATH="$PATH:/$GO_PATH/bin"
+
 protoc --go_out=../server/pb --go_opt=paths=source_relative --go-grpc_out=../server/pb --go-grpc_opt=paths=source_relative --experimental_allow_proto3_optional articles/service.proto
 protoc --go_out=../server/pb --go_opt=paths=source_relative --go-grpc_out=../server/pb --go-grpc_opt=paths=source_relative --experimental_allow_proto3_optional comments/service.proto
 protoc --go_out=../server/pb --go_opt=paths=source_relative --go-grpc_out=../server/pb --go-grpc_opt=paths=source_relative users/service.proto
